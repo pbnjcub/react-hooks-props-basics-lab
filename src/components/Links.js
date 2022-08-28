5418902b54ef8de9ce808f199de0d4f2ad674631
Paul Jung <pbnjcub@gmail.com>
2022-08-28 15:32:27 -0400

    lab done

---
+++ src/components/Links.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+function Links(props) {
+  console.log(props.github)
+  return (
+    <div id="links">
+      <h3>Links</h3>
+      <a href={props.github}>{props.github}</a>      
+      <a href={props.linkedin}>{props.linkedin}</a>
+    </div>
+  );
+}
+
+export default Links;
