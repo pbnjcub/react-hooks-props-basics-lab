e5b6dd3e02bee10cf7d4a6eae4360e529b2bcfd8
Paul Jung <pbnjcub@gmail.com>
2022-08-28 15:32:12 -0400

    commit tree a46de26ea95da65857fa1e41537a94e4ea3a388d

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+
+
 function Home(props) {
+  const homeStyle = {
+    color: props.color,
+  }
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
+      <h1 style={ homeStyle }>
         {props.name} is a Web Developer from {props.city}
       </h1>
     </div>
